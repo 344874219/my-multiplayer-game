0fbb36dfcaa43cd00d2b9a43cf4cb8b4bce0dad5
344874219 <zaka.khan@student.tdsb.on.ca>
2026-02-02 21:45:11 -0500

    server.js

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const io = require('socket.io')(http, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
 const path = require('path');
 
 app.use(express.static('public'));
@@ -15,14 +20,21 @@ setInterval(() => {
 }, 33);
 
 io.on('connection', (socket) => {
+    console.log('Player connected:', socket.id);
     players[socket.id] = { x: 0, y: 0, hp: 1000 };
+
     socket.on('update', (data) => {
         if (players[socket.id]) {
             players[socket.id].x = data.x;
             players[socket.id].y = data.y;
         }
     });
-    socket.on('disconnect', () => delete players[socket.id]);
+
+    socket.on('disconnect', () => {
+        console.log('Player disconnected:', socket.id);
+        delete players[socket.id];
+    });
 });
 
-http.listen(process.env.PORT || 3000, () => console.log('Server Live'));
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => console.log('Server Live on port ' + PORT));
