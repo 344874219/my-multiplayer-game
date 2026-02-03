fc3408138286c55152db0d405cfaa23196977d0d
344874219 <zaka.khan@student.tdsb.on.ca>
2026-02-02 21:59:18 -0500

    server.js

--- server.js
+++ server.js
@@ -1,40 +1,38 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"]
-  }
-});
-const path = require('path');
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 let players = {};
-let mobs = [];
 
-// Sync everything 30 times a second
+/* SEND GAME STATE */
 setInterval(() => {
-    io.emit('tick', { players, mobs });
+    io.emit("tick", { players });
 }, 33);
 
-io.on('connection', (socket) => {
-    console.log('Player connected:', socket.id);
-    players[socket.id] = { x: 0, y: 0, hp: 1000 };
+io.on("connection", socket => {
+    console.log("Player connected:", socket.id);
 
-    socket.on('update', (data) => {
+    players[socket.id] = {
+        x: 0,
+        y: 0
+    };
+
+    socket.on("update", data => {
         if (players[socket.id]) {
             players[socket.id].x = data.x;
             players[socket.id].y = data.y;
         }
     });
 
-    socket.on('disconnect', () => {
-        console.log('Player disconnected:', socket.id);
+    socket.on("disconnect", () => {
+        console.log("Player disconnected:", socket.id);
         delete players[socket.id];
     });
 });
 
-const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => console.log('Server Live on port ' + PORT));
+http.listen(process.env.PORT || 3000, () =>
+    console.log("Server Live")
+);
